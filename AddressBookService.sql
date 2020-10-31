27a504cd7f04ea80101c054a326e10fdfcbc1a17
jangle <shubhamjangale0000@gmail.com>
2020-10-31 10:49:11 +0530

    Added : Added functionality to create table in a database

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -6,3 +6,16 @@ create database address_book_service;
 show databases;
 use address_book_service;
 select database();
+
+#UC2
+create table address_book(
+	fname varchar(100) not null,
+	lname varchar(100) not null,
+	address varchar(200) not null,
+	city varchar(50) not null,
+	zip numeric(6) not null,
+	phone numeric(10) not null,
+	email nvarchar(255) not null,
+	primary key(fname, lname)
+);
+describe address_book;
